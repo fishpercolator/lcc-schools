b75bdb702629b1e6f36fdd7d165c7fe3e06050b0
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-08-04 14:55:36 +0100

    Basic spec for schools import

---
+++ spec/lib/schools/import/schools_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+require 'schools/import/schools'
+
+describe Schools::Import::Schools do
+  context 'no file' do
+    it 'fails' do
+      expect { Schools::Import::Schools.new('not there.csv').run! }.to raise_error(
+                                                                         Errno::ENOENT
+                                                                       )
+    end
+  end
+
+  context 'everything is fine, and everyone is happy' do
+    let(:filename) { 'spec/fixtures/import/schools.csv' }
+
+    before do
+      Schools::Import::Schools.new(filename).run!
+    end
+
+    it 'imported two schools' do
+      expect(School.all.count).to eq(4)
+    end
+
+    context 'Secondary school' do
+      subject(:school) { School.find_by(code: '5400') }
+
+      example { expect(school.name).to eql('Abbey Grange Church of England Academy') }
+      example { expect(school.phase).to eql('Secondary') }
+      example { expect(school.address).to include('Butcher Hill') }
+      example { expect(school.available_places).to eql(240) }
+      example { expect(school.not_all_nearest).to eql(false) }
+      example { expect(school.centroid).to be_kind_of(RGeo::Geos::CAPIPointImpl) }
+    end
+
+    describe 'Not all nearest allocated' do
+      subject(:school) { School.find_by(code: '2436') }
+
+      example { expect(school.name).to eql('Alwoodley Primary School') }
+      example { expect(school.phase).to eql('Primary') }
+      example { expect(school.address).to include('Cranmer Rise LS17 5HX') }
+      example { expect(school.available_places).to eql(60) }
+      example { expect(school.not_all_nearest).to eql(true) }
+      example { expect(school.centroid).to be_kind_of(RGeo::Geos::CAPIPointImpl) }
+    end
+  end
+end
