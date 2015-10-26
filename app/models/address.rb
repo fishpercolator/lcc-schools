141307b1aea793bc9f8f20dc37d416e760d051a5
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-10-26 20:01:48 +0000

    Add Address.lookup
    * Will find an address from DB first, fall back on geocoder last

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,25 @@
 class Address < ActiveRecord::Base
   geocoded_by :geocoding_info
 
+  validates_presence_of :latitude, :longitude
+
   def geocoding_info
     "#{name_or_number}, #{postcode}"
+  end
+
+  def to_s
+    geocoding_info
+  end
+
+  def self.lookup(postcode, name_or_number)
+    Address.where(
+      postcode: postcode,
+      name_or_number: name_or_number).first_or_initialize.tap do |address|
+
+      if address.new_record?
+        address.geocode
+        address.save!
+      end
+    end
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -6,4 +6,50 @@ RSpec.describe Address, type: :model do
 
     specify { expect(address.geocoding_info).to eql('79, LS8 1JU') }
   end
+
+  describe '.lookup' do
+    subject(:address) { Address.lookup('LS8 1JU', '30') }
+
+    GEOCODER_LATITUDE  = 53.0
+    GEOCODER_LONGITUDE = -1.2
+    DATABASE_LATITUDE = 50
+    DATABASE_LONGITUDE = -1
+
+    before do
+      Geocoder.configure(:lookup => :test)
+
+      Geocoder::Lookup::Test.set_default_stub(
+        [
+          {
+            'latitude'  => GEOCODER_LATITUDE,
+            'longitude' => GEOCODER_LONGITUDE,
+            'address'   => 'Test address'
+          }
+        ]
+      )
+    end
+
+    context 'postcode and name/number input are fine' do
+      context 'an address has previously been looked up' do
+        let!(:pre_existing_address) do
+          Address.create postcode: 'LS8 1JU',
+                         name_or_number: '30',
+                         latitude: DATABASE_LATITUDE,
+                         longitude: DATABASE_LONGITUDE
+        end
+
+        it 'populates lat/long from the database' do
+          expect(address.latitude).to eql(50.0)
+          expect(address.longitude).to eql(-1.0)
+        end
+      end
+
+      context 'no address has previously been looked up' do
+        it 'populates lat/long from the geocoder' do
+          expect(address.latitude).to eql(GEOCODER_LATITUDE)
+          expect(address.longitude).to eql(GEOCODER_LONGITUDE)
+        end
+      end
+    end
+  end
 end
